976eb99d469e684b3544443b7a0abb354d3c74c1
Felix Wotschofsky <felix.wotschofsky@gmx.de>
2019-07-17 14:40:02 +0200

    Store user config in memory instead of in a file

--- storage.js
+++ storage.js
@@ -1,10 +1,8 @@
 const low = require('lowdb')
 const Memory = require('lowdb/adapters/Memory')
-const FileSync = require('lowdb/adapters/FileSync')
 
 
-// const adapter = new Memory()
-const adapter = new FileSync('storage.json')
+const adapter = new Memory()
 const db = low(adapter)
 
 db.setState({})
