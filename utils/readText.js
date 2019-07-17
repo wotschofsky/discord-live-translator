6a0ade01058e2b038b96c3a02bd8445682563a2a
Felix Wotschofsky <felix.wotschofsky@gmx.de>
2019-07-17 14:42:06 +0200

    Added padding to voice output to prevent audio glitches

--- utils/readText.js
+++ utils/readText.js
@@ -1,6 +1,7 @@
 const fs = require('fs-extra')
 const path = require('path')
 
+const addAudioPadding = require('./addAudioPadding')
 const textToSpeech = require('../watson/textToSpeech')
 
 
@@ -8,6 +9,7 @@ const readText = (message, connection, voice) => {
    return new Promise((resolve) => {
       let pathName = path.join(__dirname, '..', 'cache', 'tts')
       let fileName = path.join(pathName, `${Math.round(Math.random() * 10e5)}_${Date.now()}.wav`)
+      let paddedFileName = path.join(pathName, `${Math.round(Math.random() * 10e5)}_${Date.now()}_padded.wav`)
       fs.ensureDir(pathName, () => {
          let stream = fs.createWriteStream(fileName)
          textToSpeech.synthesize({
@@ -17,13 +19,16 @@ const readText = (message, connection, voice) => {
          }).then((audio) => {
             audio.pipe(stream)
             stream.on('finish', () => {
-               console.log(`Reading: ${message}`)
-               let dispatcher = connection.playFile(fileName)
-               dispatcher.on('end', () => {
-                  resolve()
-                  setTimeout(() => {
-                     fs.remove(fileName)
-                  }, 1000)
+               addAudioPadding(fileName, paddedFileName).then(() => {
+                  console.log(`Reading: ${message}`)
+                  let dispatcher = connection.playFile(paddedFileName)
+                  dispatcher.on('end', () => {
+                     resolve()
+                     setTimeout(() => {
+                        fs.remove(fileName)
+                        fs.remove(paddedFileName)
+                     }, 1000)
+                  })
                })
             })
          })
