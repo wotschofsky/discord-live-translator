54da1208b7e37613e33bef8ca5600f9f050b80a2
Felix Wotschofsky <hello@felisk.io>
2023-05-29 13:49:57 +0200

    Remove message handlers 🧹

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ import env from './env';
 import writeToLog from './utils/writeToLog';
 
 const client = new Discord.Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES]
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES]
 });
 
 client.on('interactionCreate', async (interaction) => {
@@ -36,24 +36,6 @@ client.on('interactionCreate', async (interaction) => {
     case 'stop':
       await stopCommandHandler(interaction);
       break;
-  }
-});
-
-client.on('messageCreate', async (message) => {
-  if (
-    !message.author.bot &&
-    (message.content.trim() === '!translation' || message.content.trim().startsWith('!translation '))
-  ) {
-    if (
-      message.guild?.me
-        ?.permissionsIn(message.channel.id)
-        .has([Permissions.FLAGS.READ_MESSAGE_HISTORY, Permissions.FLAGS.SEND_MESSAGES])
-    ) {
-      message.reply(
-        'The `!translation` command is no longer supported - use slash commands instead! :x:\n' +
-          'If the commands are not available, please reinvite the bot by visiting https://discord-live-translator.felisk.io/'
-      );
-    }
   }
 });
 
