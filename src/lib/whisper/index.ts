177a0a8b3abb7efd91ae5083097f89491c95032f
Felix Wotschofsky <hello@felisk.io>
2023-07-07 01:50:39 +0200

    Offload Whisper process into worker 🚀

--- src/lib/whisper/index.ts
+++ src/lib/whisper/index.ts
@@ -1,3 +1,46 @@
-const whisper = require('../../../build/Release/whisper.node');
+import path from 'node:path';
+import { isMainThread, Worker } from 'node:worker_threads';
 
-export const whisperFull = (audioPath: string, modelPath: string): string => whisper.whisperFull(audioPath, modelPath);
+type MainToWorkerMessage = {
+  type: 'start';
+  modelPath: string;
+  audioPath: string;
+};
+
+type WorkerToMainMessage =
+  | {
+      type: 'success';
+      result: string;
+    }
+  | {
+      type: 'error';
+    };
+
+export const whisperFull = (audioPath: string, modelPath: string): Promise<string> =>
+  new Promise((resolve, reject) => {
+    if (!isMainThread) {
+      reject(new Error('whisperFull can only be called from the main thread'));
+      return;
+    }
+
+    const worker = new Worker(path.resolve(__dirname, './worker.js'));
+
+    worker.on('message', (message: WorkerToMainMessage) => {
+      switch (message.type) {
+        case 'success': {
+          resolve(message.result);
+          break;
+        }
+        case 'error': {
+          reject(new Error('Failed to recognize recording'));
+          break;
+        }
+      }
+    });
+
+    worker.postMessage({
+      type: 'start',
+      audioPath,
+      modelPath
+    } as MainToWorkerMessage);
+  });
--- src/processors/recognizeRecording.ts
+++ src/processors/recognizeRecording.ts
@@ -9,7 +9,7 @@ const modelPath = path.join(__dirname, '../../models/ggml-small.bin');
 const recognizeRecording = async (fileName: string) => {
   writeToLog(`Analyzing "${fileName}"...`);
 
-  const result = whisperFull(fileName, modelPath);
+  const result = await whisperFull(fileName, modelPath);
 
   fs.unlink(fileName).catch((err) => {
     console.error(`Failed to delete ${fileName} - ${err}`);
